abb923ca4613886f7083abe3ad551d8b9cd0141c
Pavel Lunev <pavellunev>
2022-10-09 20:26:39 +0300

    Initial Commit

---
+++ Telegram_IOS_Contest_2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Telegram_IOS_Contest_2022
+//
+//  Created by Павел Лунев on 09.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
